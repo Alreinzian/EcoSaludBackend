6552688bfec9c02e77fa6c3ad62fc0a1d21f5bda
EduardoP103 <eduardopastor103@gmail.com>
2023-07-09 09:26:29 -0500

    Se agrega el CorsFilter

--- src/main/java/com/ecoSalud/springboot/app/controllers/DoctorController.java
+++ src/main/java/com/ecoSalud/springboot/app/controllers/DoctorController.java
@@ -19,43 +19,43 @@ import com.ecoSalud.springboot.app.services.DoctorService;
 @Controller
 @RequestMapping("/doctor")
 public class DoctorController {
-	@Autowired
-	private DoctorService doctorService;
+    @Autowired
+    private DoctorService doctorService;
 
-	@RequestMapping("/listar")
-	public String listar(Model model) {
+    @RequestMapping("/listar")
+    public String listar(Model model) {
 
-		List<Doctor> listaDoctores = doctorService.buscarTodo();
-		System.out.println("LISTA DE DOCTORES: " + listaDoctores);
-		model.addAttribute("listaDoctores", listaDoctores);
-		return "moduloDoctor/listar";
-	}
+        List<Doctor> listaDoctores = doctorService.buscarTodo();
+        System.out.println("LISTA DE DOCTORES: " + listaDoctores);
+        model.addAttribute("listaDoctores", listaDoctores);
+        return "moduloDoctor/listar";
+    }
 
-	@RequestMapping("/crear")
-	public String crear(Model model) {
-		Doctor doctor = new Doctor();
-		model.addAttribute("doctor", doctor);
-		return "moduloDoctor/crear";
-	}
+    @RequestMapping("/crear")
+    public String crear(Model model) {
+        Doctor doctor = new Doctor();
+        model.addAttribute("doctor", doctor);
+        return "moduloDoctor/crear";
+    }
 
-	@RequestMapping(value = "/guardar", method = RequestMethod.POST)
-	public String crear(@ModelAttribute("doctor") Doctor doctor) {
-		doctorService.crear(doctor);
-		return "redirect:/doctor/listar";
-	}
+    @RequestMapping(value = "/guardar", method = RequestMethod.POST)
+    public String crear(@ModelAttribute("doctor") Doctor doctor) {
+        doctorService.crear(doctor);
+        return "redirect:/doctor/listar";
+    }
 
-	@RequestMapping(value = "/actualizar/{id}", method = RequestMethod.GET)
-	public ModelAndView editar(@PathVariable(name = "id") Integer id) {
-		ModelAndView mav = new ModelAndView("moduloDoctor/form");
-		Doctor doctor = doctorService.buscarPorId(id);
-		mav.addObject("doctor", doctor);
-		return mav;
-	}
+    @RequestMapping(value = "/actualizar/{id}", method = RequestMethod.GET)
+    public ModelAndView editar(@PathVariable(name = "id") Integer id) {
+        ModelAndView mav = new ModelAndView("moduloDoctor/form");
+        Doctor doctor = doctorService.buscarPorId(id);
+        mav.addObject("doctor", doctor);
+        return mav;
+    }
 
-	@RequestMapping(value = "/eliminar/{id}", method = RequestMethod.GET)
-	public String eliminar(@PathVariable(name = "id") Integer id) {
-		doctorService.eliminarDoctor(id);
-		return "redirect:/doctor/listar";
-	}
+    @RequestMapping(value = "/eliminar/{id}", method = RequestMethod.GET)
+    public String eliminar(@PathVariable(name = "id") Integer id) {
+        doctorService.eliminarDoctor(id);
+        return "redirect:/doctor/listar";
+    }
 
 }
